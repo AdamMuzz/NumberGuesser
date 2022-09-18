09a81b2edbd5b6817d2260113925955493651b92
Adam Muzzarelli <admuzzarelli@gmail.com>
2022-09-18 12:50:12 -0700

    backend runs ML code w/ frontend input

--- ML/api.py
+++ ML/api.py
@@ -1,13 +1,22 @@
 from tensorflow import keras
 import numpy as np
+import json, sys
 
 '''
 being executed from nodejs subprocess
-root is /backend folder
+root is therefore /backend folder
 '''
+
+#grab canvas data from command line args and preprocess it
+input = json.loads(sys.argv[1])
+input = np.array(input)
+input = input / 255
+input = input.reshape(1,784)
 
 #grab model from save
 model = keras.models.load_model('../model')
 
-print('loaded model successfully')
-
+#run model on input
+prediction = model.predict(input)
+rez = np.argmax(prediction)
+print(f'{rez}: confidence {prediction[0][rez]:.3f}')
--- backend/subprocess.js
+++ backend/subprocess.js
@@ -1,9 +1,10 @@
 const { spawn } = require('node:child_process');
 
-function run_subprocess(args) {
-	console.log(args)
+function run_subprocess(input) {
+	const input_json = JSON.stringify(input)
+	//console.log(input_json);
 
-	const args = ['../ML/api.py'] //arguments for python to execute
+	const args = ['../ML/api.py', input_json] //arguments for python to execute
 	const process = spawn('/Users/admuzz/Desktop/NumberGuesser/venv/bin/python', args); //execute virtualenv python w/ above args
 
 	//collect outout from subprocess
